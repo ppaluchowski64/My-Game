e471c8bf75d1db123e2cdb484c5743b5385b1000
Pablo Majster <104451887+ppaluchowski64@users.noreply.github.com>
2025-04-22 21:22:39 +0200

    Create animations for player character

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name PlayerController
 
 @export var speed = 10.0
 @export var jump_power = 10.0
@@ -9,7 +10,6 @@ var direction = 0
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
